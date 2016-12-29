2f9cb16b5e6a5f5d0f8f253785fb72d00c3fcd8d
bunsim <bunsim@protonmail.com>
2016-12-29 11:46:24 -0500

    target newest binaries

--- UPDATE-DAEMON.sh
+++ UPDATE-DAEMON.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 echo "Updating Windows binary..."
-curl https://dl.geph.io/XGO_BUILD/geph-v0.1.2-windows-4.0-386.exe > ./assets/binaries/windows-i386/geph.exe
+curl https://dl.geph.io/XGO_BUILD/geph-v0.1.4-windows-4.0-386.exe > ./assets/binaries/windows-i386/geph.exe
 
 echo "Updating Linux 64-bit binary..."
-curl https://dl.geph.io/XGO_BUILD/geph-v0.1.2-linux-amd64 > ./assets/binaries/linux-amd64/geph
+curl https://dl.geph.io/XGO_BUILD/geph-v0.1.4-linux-amd64 > ./assets/binaries/linux-amd64/geph
 chmod +x ./assets/binaries/linux-amd64/geph
 
 echo "Updating Linux 32-bit binary..."
-curl https://dl.geph.io/XGO_BUILD/geph-v0.1.2-linux-386 > ./assets/binaries/linux-i386/geph
+curl https://dl.geph.io/XGO_BUILD/geph-v0.1.4-linux-386 > ./assets/binaries/linux-i386/geph
 chmod +x ./assets/binaries/linux-i386/geph
 
 echo "Updating macOS 64-bit binary..."
-curl https://dl.geph.io/XGO_BUILD/geph-v0.1.2-darwin-10.6-amd64 > ./assets/binaries/darwin-amd64/geph
+curl https://dl.geph.io/XGO_BUILD/geph-v0.1.4-darwin-10.6-amd64 > ./assets/binaries/darwin-amd64/geph
 chmod +x ./assets/binaries/darwin-amd64/geph
